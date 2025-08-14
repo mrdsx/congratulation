c429053189c8d18e1393a6d0977db992f34253cc
mrdsx <andre0.castlegames@gmail.com>
2025-08-14 23:51:11 +0300

    add files

---
+++ script.js
@@ -0,0 +1,19 @@
+const after = document.querySelector(".after");
+const audioComponent = document.querySelector(".audio-component");
+const before = document.querySelector(".before");
+const congratulation = document.querySelector(".congratulation");
+const pressMeBtn = document.querySelector(".press-me-btn");
+
+congratulation.style.display = "none";
+after.style.display = "none";
+before.style.display = "none";
+
+pressMeBtn.addEventListener("click", () => {
+  pressMeBtn.style.display = "none";
+
+  congratulation.style.display = "block";
+  after.style.display = "block";
+  before.style.display = "block";
+
+  // audioComponent.play();
+});
